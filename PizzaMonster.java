7c411baf530b3744604e8c3a145d6867c3b5bdc5
JgrabowskiUMW <jgrabowk@mail.umw.edu>
2022-03-30 22:28:08 -0400

    comment

--- PizzaMonster.java
+++ PizzaMonster.java
@@ -6,6 +6,7 @@ public class PizzaMonster {
     public static void main(String[] args) {
 
         frame = new JFrame("Lab 12: GUI Exercise");
+        //set size of window
         frame.setSize(350, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
