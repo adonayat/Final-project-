2b445e2b45b97bc7e7787c523bc4de96ff631d6d
JgrabowskiUMW <jgrabowk@mail.umw.edu>
2022-03-30 21:31:14 -0400

    make frame set size of frame make frame close on exit

--- PizzaMonster.java
+++ PizzaMonster.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class PizzaMonster {
+    private static JFrame frame;
+
+    public static void main(String[] args) {
+
+        frame = new JFrame("Lab 12: GUI Exercise");
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
