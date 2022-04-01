d6dd4b407dcbdc73462affe01881cb8ed7b25231
JgrabowskiUMW <jgrabowk@mail.umw.edu>
2022-03-31 23:15:35 -0400

    make template for tabs

--- PizzaMonster.java
+++ PizzaMonster.java
@@ -9,5 +9,21 @@ public class PizzaMonster {
         //set size of window
         frame.setSize(350, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //create menu
+        JMenuBar menuBar = new JMenuBar();
+        // add the file menu
+        JMenu file = new JMenu("File");
+        //make the Exit tab
+        JMenuItem exit = new JMenuItem("Exit", null);
+        file.add(exit);
+        // add file to the menubar
+        menuBar.add(file);
+        // add the menuBar to the window
+        frame.setJMenuBar(menuBar);
+        // set other things
+        frame.setTitle("Simple menu");
+        // launch the windwo
+        frame.setVisible(true);
+
     }
 }
